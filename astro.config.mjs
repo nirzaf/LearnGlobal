318e5f19766f44925f0fdfb597239571e6b09771
M.F.M Fazrin <mfmfazrin1986@gmail.com>
2024-10-31 13:07:12 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://dotnetevangelist.com',
+  trailingSlash: 'never',
+  base:'LearnGlobal',
   integrations: [tailwind({
     applyBaseStyles: false
   }), react()],
+  outDir: './dist'
 });
